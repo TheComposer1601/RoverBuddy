94e433c4615ed99adc4d58d65e06918bc878c901
Ryan Garcia <megamanbn@live.com>
2015-02-23 22:25:54 -0700

    Movement System
    Movement should be operational

--- RoverBuddy/src/SensorWrappers/MyMovement.java
+++ RoverBuddy/src/SensorWrappers/MyMovement.java
@@ -1,5 +1,15 @@
 package SensorWrappers;
 
-public class MyMovement {
-	
+import lejos.nxt.MotorPort;
+import SensorWrapperInterface.MovementLejosInterface;
+
+public class MyMovement implements MovementLejosInterface{
+	MotorPort motor;
+	public MyMovement(MotorPort port){
+		motor = port;
+	}
+	@Override
+	public void controlMotor(int power, int mode) {
+		motor.controlMotor(power, mode);
+	}
 }
